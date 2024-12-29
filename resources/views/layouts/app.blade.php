dfeff0430aec8c9db78dfa3531e991cdbd53c426
Abdul-Razak Ibrahim <33864119+razachisibrahimah@users.noreply.github.com>
2024-12-28 22:47:08 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,8 +55,8 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
+<!--                             <li><a href="{{ route('register') }}">Register</a></li>
+                        @else -->
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
