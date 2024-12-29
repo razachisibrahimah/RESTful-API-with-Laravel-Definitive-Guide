959e0d7389f68aa3cde060ee91974107bc4b0b5c
Abdul-Razak Ibrahim <33864119+razachisibrahimah@users.noreply.github.com>
2024-12-28 22:47:57 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('register', 'User\UserController@showRegisterForm')->name('register')->middleware('guest');
+// Route::get('register', 'User\UserController@showRegisterForm')->name('register')->middleware('guest');
 Route::post('register', 'User\UserController@store')->middleware('guest');
 
 // Password Reset Routes...
